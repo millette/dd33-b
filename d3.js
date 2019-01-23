0f777a04b06f41ede9e9aab492d480d91b70ab5f
Robin Millette <robin@millette.info>
2019-01-23 16:44:52 -0500

    d3 instance.

---
+++ d3.js
@@ -0,0 +1,27 @@
+import {
+  select,
+  scaleOrdinal,
+  schemeCategory10,
+  forceSimulation,
+  forceManyBody,
+  forceLink,
+  forceX,
+  forceY,
+  timeout,
+  interval,
+  now,
+} from "d3"
+
+export default {
+  select,
+  scaleOrdinal,
+  schemeCategory10,
+  forceSimulation,
+  forceManyBody,
+  forceLink,
+  forceX,
+  forceY,
+  timeout,
+  interval,
+  now,
+}

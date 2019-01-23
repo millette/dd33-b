d7151979c0ce3bbaf6bc7735d9b19dfeb3079f7c
Robin Millette <robin@millette.info>
2019-01-23 18:50:16 -0500

    Clean up.

--- d3.js
+++ d3.js
@@ -1,7 +1,7 @@
 import { select } from "d3-selection"
-
 import { scaleOrdinal } from "d3-scale"
 import { schemeCategory10 } from "d3-scale-chromatic"
+import { timeout, interval, now } from "d3-timer"
 import {
   forceLink,
   forceX,
@@ -9,23 +9,6 @@ import {
   forceManyBody,
   forceSimulation,
 } from "d3-force"
-import { timeout, interval, now } from "d3-timer"
-
-/*
-import {
-  select,
-  scaleOrdinal,
-  schemeCategory10,
-  forceSimulation,
-  forceManyBody,
-  forceLink,
-  forceX,
-  forceY,
-  timeout,
-  interval,
-  now,
-} from "d3"
-*/
 
 export default {
   select,

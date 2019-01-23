295c0539bc32a4a5da202ffb0b1f912c80b6c1be
Robin Millette <robin@millette.info>
2019-01-23 18:43:56 -0500

    Smaller build.

--- d3.js
+++ d3.js
@@ -1,3 +1,17 @@
+import { select } from "d3-selection"
+
+import { scaleOrdinal } from "d3-scale"
+import { schemeCategory10 } from "d3-scale-chromatic"
+import {
+  forceLink,
+  forceX,
+  forceY,
+  forceManyBody,
+  forceSimulation,
+} from "d3-force"
+import { timeout, interval, now } from "d3-timer"
+
+/*
 import {
   select,
   scaleOrdinal,
@@ -11,6 +25,7 @@ import {
   interval,
   now,
 } from "d3"
+*/
 
 export default {
   select,

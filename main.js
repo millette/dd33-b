86621fd052840f81dd4d6ee266558a65ff1477c0
Robin Millette <robin@millette.info>
2019-01-23 19:01:33 -0500

    Clean up js.

--- main.js
+++ main.js
@@ -7,28 +7,10 @@ const width = +svg.attr("width")
 const height = +svg.attr("height")
 const color = d3.scaleOrdinal(d3.schemeCategory10)
 
-/*
-const a = { id: "a" }
-const b = { id: "b" }
-const c = { id: "c" }
-const d = { id: "d" }
-
-const nodes = [a, b, c, d]
-// const links = []
-const links = [
-  { source: a, target: b },
-  { source: d, target: c }
-]
-*/
-
 import stuff from "./el-graph-data.json"
 
-const nodes = stuff.nodes // .map((x) => ({...x}))
+const nodes = stuff.nodes
 const links = []
-// const links = stuff.links.map(({source, target}) => ({ source: {id: source}, target: {id : target}}))
-
-console.log("NODES", nodes.length)
-console.log("LINKS", links.length, links[0])
 
 const simulation = d3
   .forceSimulation(nodes)
@@ -58,108 +40,38 @@ let node = g
 restart()
 
 d3.timeout(function() {
-  // links = stuff.links.map(({source, target}) => ({ source: {id: source}, target: {id : target}}))
   const source = nodes[0]
-
-  /*
-  console.log('SOURCE', source)
-
-  const target = nodes[1]
-  console.log('TARGET', target)
-  links.push({ source, target })
-  */
 
   stuff.links.slice(15).forEach((z, i) => {
     const target = nodes[i + 1]
-    // console.log('TARGET', target)
-    if (!target) return
-    links.push({ source, target })
+    if (target) links.push({ source, target })
   })
-  // stuff.links.forEach(({source, target}) => links.push({ source, target }))
-  console.log("LINKS", links.length, links.slice(0)[0])
   restart()
 }, 200)
 
 d3.timeout(function() {
-  // links = stuff.links.map(({source, target}) => ({ source: {id: source}, target: {id : target}}))
   const source = nodes[25]
-
-  /*
-  console.log('SOURCE', source)
-
-  const target = nodes[1]
-  console.log('TARGET', target)
-  links.push({ source, target })
-  */
 
   stuff.links.forEach((z, i) => {
     const target = nodes[i + 30]
-    // console.log('TARGET', target)
-    if (!target) return
-    links.push({ source, target })
+    if (target) links.push({ source, target })
   })
-  // stuff.links.forEach(({source, target}) => links.push({ source, target }))
-  console.log("LINKS", links.length, links.slice(0)[0])
   restart()
-}, 1200)
+}, 500)
 
 d3.timeout(function() {
-  // links = stuff.links.map(({source, target}) => ({ source: {id: source}, target: {id : target}}))
   const source = nodes[15]
-
-  /*
-  console.log('SOURCE', source)
-
-  const target = nodes[1]
-  console.log('TARGET', target)
-  links.push({ source, target })
-  */
 
   stuff.links.forEach((z, i) => {
     const target = nodes[i + 20]
-    // console.log('TARGET', target)
-    if (!target) return
-    links.push({ source, target })
+    if (target) links.push({ source, target })
   })
-  // stuff.links.forEach(({source, target}) => links.push({ source, target }))
-  console.log("LINKS", links.length, links.slice(0)[0])
   restart()
-}, 2000)
+}, 1000)
 
 d3.timeout(function() {
   simulation.stop()
 }, 50000)
-
-/*
-d3.timeout(function() {
-  links.push({ source: a, target: b }) // Add a-b.
-  links.push({ source: b, target: c }) // Add b-c.
-  links.push({ source: c, target: a }) // Add c-a.
-  restart()
-}, 1000)
-
-d3.interval(
-  function() {
-    nodes.pop() // Remove c.
-    links.pop() // Remove c-a.
-    links.pop() // Remove b-c.
-    restart()
-  },
-  2000,
-  d3.now()
-)
-
-d3.interval(
-  function() {
-    nodes.push(c) // Re-add c.
-    links.push({ source: b, target: c }) // Re-add b-c.
-    links.push({ source: c, target: a }) // Re-add c-a.
-    restart()
-  },
-  2000,
-  d3.now() + 1000
-)
-*/
 
 function restart() {
   // Apply the general update pattern to the nodes.
@@ -193,25 +105,11 @@ function restart() {
 }
 
 function ticked() {
-  node
-    .attr("cx", function(d) {
-      return d.x
-    })
-    .attr("cy", function(d) {
-      return d.y
-    })
+  node.attr("cx", (d) => d.x).attr("cy", (d) => d.y)
 
   link
-    .attr("x1", function(d) {
-      return d.source.x
-    })
-    .attr("y1", function(d) {
-      return d.source.y
-    })
-    .attr("x2", function(d) {
-      return d.target.x
-    })
-    .attr("y2", function(d) {
-      return d.target.y
-    })
+    .attr("x1", (d) => d.source.x)
+    .attr("y1", (d) => d.source.y)
+    .attr("x2", (d) => d.target.x)
+    .attr("y2", (d) => d.target.y)
 }

fafb4452e716ba1654463f4a3e34b40ffefec09c
Robin Millette <robin@millette.info>
2019-01-24 15:59:47 -0500

    Handle new users and duplicates.

--- main.js
+++ main.js
@@ -7,10 +7,11 @@ const width = +svg.attr("width")
 const height = +svg.attr("height")
 const color = d3.scaleOrdinal(d3.schemeCategory10)
 
-import stuff from "./el-graph-data.json"
+// import stuff from "./el-graph-data.json"
 
-const nodes = stuff.nodes
-const links = []
+// const nodes = stuff.nodes
+const dataNodes = []
+const dataLinks = []
 
 const g = svg
   .append("g")
@@ -39,9 +40,9 @@ const ticked = () => {
 }
 
 const simulation = d3
-  .forceSimulation(nodes)
-  .force("charge", d3.forceManyBody().strength(-150))
-  .force("link", d3.forceLink(links).distance(50))
+  .forceSimulation(dataNodes)
+  .force("charge", d3.forceManyBody().strength(-125))
+  .force("link", d3.forceLink(dataLinks).distance(200))
   .force("x", d3.forceX())
   .force("y", d3.forceY())
   .alphaTarget(1)
@@ -58,7 +59,7 @@ const makeUser = (d) => {
 const restart = () => {
   // Apply the general update pattern to the nodes.
   // node = node.data(nodes, (d) => d.id)
-  node = node.data(nodes)
+  node = node.data(dataNodes)
   node.exit().remove()
   node = node
     .enter()
@@ -69,7 +70,7 @@ const restart = () => {
     .merge(node)
 
   // Apply the general update pattern to the links.
-  link = link.data(links, (d) => d.source.id + "-" + d.target.id)
+  link = link.data(dataLinks, (d) => d.source.id + "-" + d.target.id)
   link.exit().remove()
   link = link
     .enter()
@@ -77,46 +78,58 @@ const restart = () => {
     .merge(link)
 
   // Update and restart the simulation.
-  simulation.nodes(nodes)
-  simulation.force("link").links(links)
+  simulation.nodes(dataNodes)
+  simulation.force("link").links(dataLinks)
   simulation.alpha(1).restart()
 }
 
 restart()
 
-const fakeIt = () => {
-  d3.timeout(() => {
-    const source = nodes[0]
+const hereRe = /(montréal|montreal|mtl|yul)/
 
-    stuff.links.slice(15).forEach((z, i) => {
-      const target = nodes[i + 1]
-      if (target) links.push({ source, target })
-    })
-    restart()
-  }, 200)
-
-  d3.timeout(() => {
-    const source = nodes[25]
-
-    stuff.links.forEach((z, i) => {
-      const target = nodes[i + 30]
-      if (target) links.push({ source, target })
-    })
-    restart()
-  }, 500)
-
-  d3.timeout(() => {
-    const source = nodes[15]
-
-    stuff.links.forEach((z, i) => {
-      const target = nodes[i + 20]
-      if (target) links.push({ source, target })
-    })
-    restart()
-  }, 1000)
+const addUser = (username) => {
+  const found = dataNodes.find((x) => x.id === username)
+  if (found) return found
+  const it = { id: username }
+  dataNodes.push(it)
+  return it
 }
 
-fakeIt()
+const addLink = (source, target) =>
+  !dataLinks.find(
+    (x) => x.source.id === source.id && x.target.id === target.id
+  ) && dataLinks.push({ source, target })
 
-// stop after 50 seconds
-d3.timeout(() => simulation.stop(), 3000)
+const fetchOne = (oyUrl, re) => {
+  return fetch(oyUrl)
+    .then((res) => res.json())
+    .then(({ data: { user: { login, followers: { nodes } } } }) => {
+      const source = addUser(login)
+      nodes.forEach(
+        (x, i) =>
+          x.location &&
+          re.test(x.location.toLowerCase()) &&
+          addLink(source, addUser(x.login))
+      )
+      console.log("dataNodes.length:", dataNodes.length)
+      console.log("dataLinks.length:", dataLinks.length)
+      restart()
+    })
+}
+
+fetchOne(require("./oy.json"), hereRe).catch((e) => {
+  console.error(e)
+  simulation.stop()
+})
+
+const allRe = /(quebec|québec)/
+
+setTimeout(() => {
+  fetchOne(require("./oy.json"), allRe).catch((e) => {
+    console.error(e)
+    simulation.stop()
+  })
+}, 1000)
+
+// stop after a little while
+d3.timeout(() => simulation.stop(), 10000)

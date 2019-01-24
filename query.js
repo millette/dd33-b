2011027cd99b260efece23bb89a597aadd36704e
Robin Millette <robin@millette.info>
2019-01-24 18:37:37 -0500

    Query github.

---
+++ query.js
@@ -0,0 +1,22 @@
+// npm
+import { print } from "graphql/language/printer"
+
+// self
+import followsQuery from "./user-followers.graphql"
+
+// export default print(followsQuery)
+
+const query = print(followsQuery)
+
+const opts = { method: "POST", mode: "cors" }
+const headers = { "Content-Type": "application/json" }
+
+export default (token, login) =>
+  fetch("https://api.github.com/graphql", {
+    ...opts,
+    headers: {
+      ...headers,
+      authorization: `bearer ${token}`,
+    },
+    body: JSON.stringify({ query, variables: { login } }),
+  }).then((res) => res.json())
